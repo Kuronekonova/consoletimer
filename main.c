9dd3d96f3f53141fc1430b4f22302e9d80450ba2
Kuro <68709896+Kuronekonova@users.noreply.github.com>
2024-10-02 19:59:41 +0100

    Changed overall structure of code, again.

--- main.c
+++ main.c
@@ -1,53 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-enum program_options {TIMER = 1, STOPWATCH = 2}; 
-typedef int *USER_OUTPUT, *HOUR, *MINUTE, *SECOND; 
+enum program_options {TIMER = 1, STOPWATCH = 2, QUIT = 3, SETTINGS = 4}; 
+typedef int *TIME_UNIT, *USER_OUTPUT; typedef char *ZERO_CHAR;
 typedef struct{
-    HOUR hour; 
-    MINUTE minute; 
-    SECOND second;
+    TIME_UNIT time_unit_amount;
+    ZERO_CHAR zero_char;
+} *time_unit;
+typedef struct{
+    time_unit hour, minute, second;
 } *timer_params;
-// inconvenient solution. make it better.
-void print_time(timer_params timer_params){
-    char hour_zero = '\0', minute_zero = '\0', second_zero = '\0';
-    if(*(timer_params->hour) < 10){
-        hour_zero = '0';
-    }
-    if(*(timer_params->minute) < 10){
-        minute_zero = '0';
-    }
-    if(*(timer_params->second) < 10){
-        second_zero = '0';
-    }
-    printf("%c%d:%c%d:%c%d\n", hour_zero, *(timer_params->hour), minute_zero, *(timer_params->minute), second_zero, *(timer_params->second));
+static timer_params timer_params_mem(timer_params timer_params){
+    timer_params->hour = malloc(sizeof(time_unit));
+    timer_params->minute = malloc(sizeof(time_unit));
+    timer_params->second = malloc(sizeof(time_unit));
+    timer_params->hour->time_unit_amount = malloc(sizeof(int));
+    timer_params->minute->time_unit_amount = malloc(sizeof(int));
+    timer_params->second->time_unit_amount = malloc(sizeof(int));
+    timer_params->hour->zero_char = malloc(sizeof(char));
+    timer_params->minute->zero_char = malloc(sizeof(char));
+    timer_params->second->zero_char = malloc(sizeof(char));
+}
+static timer_params print_time(timer_params timer_params){
+    *timer_params->hour->zero_char = (*timer_params->hour->time_unit_amount < 10) ? '0' : '\0';
+    *timer_params->minute->zero_char = (*timer_params->minute->time_unit_amount < 10) ? '0' : '\0';
+    *timer_params->second->zero_char = (*timer_params->second->time_unit_amount < 10) ? '0' : '\0';
+    printf("%c%d:%c%d:%c%d\n", *timer_params->hour->zero_char, *(timer_params->hour->time_unit_amount), *timer_params->minute->zero_char, *(timer_params->minute->time_unit_amount), *timer_params->second->zero_char, *(timer_params->second->time_unit_amount));
     sleep(1);
 }
-void timer_stopwatch(timer_params timer_params, int user_output){
-    switch (user_output)
+static timer_params timer_stopwatch(timer_params timer_params, USER_OUTPUT user_output){
+    switch (*user_output)
     {
     case TIMER:
-        (*(timer_params->second))--;
-        if(*(timer_params->second) == - 1 && *(timer_params->minute) != 0){
-            *(timer_params->second) = 59;
-            (*(timer_params->minute))--;
+        (*(timer_params->second->time_unit_amount))--;
+        if((*(timer_params->second->time_unit_amount)) == - 1 && (*(timer_params->minute->time_unit_amount)) != 0){
+            (*(timer_params->second->time_unit_amount)) = 59;
+            (*(timer_params->minute->time_unit_amount))--;
         }
-        if(*(timer_params->second) == - 1 && *(timer_params->minute) == 0){
-            *(timer_params->hour) = (*(timer_params->hour) == -1) ? 0 : *(timer_params->hour) - 1; 
-            *(timer_params->second) = 59;
-            *(timer_params->minute) = 59;
+        if((*(timer_params->second->time_unit_amount)) == - 1 && (*(timer_params->minute->time_unit_amount)) == 0){
+            (*(timer_params->second->time_unit_amount)) = 59;
+            (*(timer_params->minute->time_unit_amount)) = 59;
+            (*(timer_params->hour->time_unit_amount)) = ((*(timer_params->hour->time_unit_amount)) == -1) ? 0 : (*(timer_params->hour->time_unit_amount)) - 1; 
         }
         break;
     case STOPWATCH:
-        (*(timer_params->second))++;
-        if(*(timer_params->second) == 60){
-            *(timer_params->second) = 0;
-            *(timer_params->minute)++;
+        (*(timer_params->second->time_unit_amount))++;
+        if(*(timer_params->second->time_unit_amount) == 60){
+            *(timer_params->second->time_unit_amount) = 0;
+            *(timer_params->minute->time_unit_amount)++;
         }
-        if(*(timer_params->minute) == 60){
-            *(timer_params->minute) = 0;
-            *(timer_params->hour)++;
+        if(*(timer_params->minute->time_unit_amount) == 60){
+            *(timer_params->minute->time_unit_amount) = 0;
+            *(timer_params->hour->time_unit_amount)++;
         }
         break;
     default:
@@ -55,39 +59,38 @@ void timer_stopwatch(timer_params timer_params, int user_output){
     }
 }
 void main(){
-    timer_params timer_params = malloc(sizeof(timer_params)); 
-    timer_params->hour = malloc(sizeof(int));
-    timer_params->minute = malloc(sizeof(int));
-    timer_params->second = malloc(sizeof(int));
+    timer_params timer_params;
+    timer_params_mem(timer_params);
     USER_OUTPUT user_output = malloc(sizeof(int));
     printf("1. Timer\n"
            "2. Stopwatch\n"
+           "3. Quit\n"
            "Your choice: ");
     scanf("%d", user_output);
     switch(*user_output){
         case TIMER:
             while(1){
                 printf("Hour(s): ");
-                scanf("%d", timer_params->hour);
+                scanf("%d", timer_params->hour->time_unit_amount);
                 printf("Minute(s): ");
-                scanf("%d", timer_params->minute);
+                scanf("%d", timer_params->minute->time_unit_amount);
                 printf("Second(s): ");
-                scanf("%d", timer_params->second);
-                if(*timer_params->minute < 0 || *timer_params->minute > 59 || *timer_params->second < 0 || *timer_params->second > 59){
+                scanf("%d", timer_params->second->time_unit_amount);
+                if(*timer_params->minute->time_unit_amount < 0 || *timer_params->minute->time_unit_amount > 59 || *timer_params->second->time_unit_amount < 0 || *timer_params->second->time_unit_amount > 59){
                     printf("Make sure your seconds and/or minutes don't go over 59 or under 0.");
                 }
                 else break;
             }
             break;
         case STOPWATCH:
-            *timer_params->hour = 0;
-            *timer_params->minute = 0;
-            *timer_params->second = 0;
-        default:
-            break;
+            *timer_params->hour->time_unit_amount = 0;
+            *timer_params->minute->time_unit_amount = 0;
+            *timer_params->second->time_unit_amount = 0;
+        case QUIT: exit(1);
+        default: break;
     }    
-    while((*user_output == TIMER) ? (*timer_params->hour != 0 || *timer_params->minute != 0 || *timer_params->second != 0) : 1){
+    while((*user_output == TIMER) ? (*timer_params->hour->time_unit_amount != 0 || *timer_params->minute->time_unit_amount != 0 || *timer_params->second->time_unit_amount != 0) : 1){
         print_time(timer_params);
-        timer_stopwatch(timer_params, *user_output);
-    }
+        timer_stopwatch(timer_params, user_output);
+    } 
 }
